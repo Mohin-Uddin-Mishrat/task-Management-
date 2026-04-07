4915f8f5d4e98dd232039505945c051ec5ab2320
mohin <mmishrat90@gmail.com>
2026-04-07 20:08:24 +0600

    update-docker-file

--- Dockerfile
+++ Dockerfile
@@ -39,4 +39,4 @@ COPY --from=builder /app/prisma.config.ts ./prisma.config.ts
 
 EXPOSE 5000
 
-CMD ["sh", "-c", "npx prisma migrate deploy && node dist/main"]
+CMD ["sh", "-c", "npx prisma migrate deploy && node dist/src/main"]
--- src/main.ts
+++ src/main.ts
@@ -21,7 +21,7 @@ async function bootstrap() {
       'http://localhost:5173',
       'http://localhost:3000',
       'http://localhost:5174',
-      'https://quick-hire-frontend-two.vercel.app',
+      'https://user-task-management-frontend-peach.vercel.app',
       '*'
     ],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
